ec342f1690744afe3370b30f77dd0a0042b2a9b8
abel ashenafi <ashenafiabel66@gmail.com>
2025-08-27 15:48:32 +0300

    feat: create tables

--- Alpha/database/migrations/tenant/2025_08_08_184335_create__leave_policys_table.php
+++ Alpha/database/migrations/tenant/2025_08_08_184335_create__leave_policys_table.php
@@ -6,13 +6,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('payrolls', function (Blueprint $table) {
+        Schema::create('_leave_policys', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('payrolls');
+        Schema::dropIfExists('_leave_policys');
     }
 };
